6d7df5c80b2c8cfb60ae9de5df532ae4d3d01720
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-10-16 19:04:57 -0400

    create user card

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const API= 'https://api.github.com/users/'
+const APIURL= 'https://api.github.com/users/'
+
+async function getUser(){
+    const resp = await fetch(APIURL + user);
+    const respData = await resp.json();
+
+    console.log(respData);
+    createUserCard(respData);
+}
+function createUserCard(){
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+    <div>
+        <img src="${user.avatar_url}" alt="${user.name}">
+    </div>
+    <div>
+        <h2>
+        ${user.name}
+        </h2>
+        <p>${user.bio}</p>
+        <ul>
+        <li> ${user.followers}</li>
+        <li> ${user.following}</li>
+        <li> ${user.public_repos}</li>
+        </ul>
+    </div>
+    `
+}
